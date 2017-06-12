839397b1e8eff6dd2028a0db05291ffad898c656
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-06-11 19:00:28 -0500

    random spooky fun :-D

--- loop.py
+++ loop.py
@@ -1,6 +1,8 @@
 import pprint
 from time import sleep
 import mido
+from mingus.containers import Note
+
 
 class Loop:
     def __init__(self, roll, scale, instrument, name):
@@ -10,36 +12,58 @@ class Loop:
         self.name = name
 
     def play(self, t):
-        for i in range(0,len(self.roll[t])):
-            if self.roll[t][i]:
-                if not self.roll[t-1][i]:
+        tt = t % len(self.roll)
+        for i in range(0,len(self.roll[tt])):
+            if self.roll[tt][i]:
+                if not self.roll[tt-1][i]:
                     # previous is 0, current is 1
                     self.instrument.send(mido.Message('note_on',
                                                       note=self.scale[i],
                                                       velocity=64))
             else:
-                if self.roll[t-1][i]:
+                if self.roll[tt-1][i]:
                     # previous is 1, current is 0
                     self.instrument.send(mido.Message('note_off',
                                                       note=self.scale[i]))
 
+    def mute(self):
+        for i in range(0,len(self.roll[0])):
+            self.instrument.send(mido.Message('note_off',
+                                              note=self.scale[i]))
+        
     def render(self, t):
-        print(self.name, self.roll[t])
+        tt = t % len(self.roll)        
+        print(self.name, self.roll[tt])
 
 
 class Sequencer:
-    loops = []
-    bpm = 111
-    delay = 60.0 / bpm
+
+    def __init__(self, loops=[], bpm=30):
+        self.loops = loops
+        bpm = bpm
+        self.delay = 60.0 / bpm
+
+        # find longest loop
+        self.longest = max([len(l.roll) for l in self.loops])
 
     def play(self):
-        t = 0
-        while True:
+        for t in range(self.longest):
             for l in self.loops:
                 l.play(t)
                 l.render(t)
             # as time goes by
-            t+=1
-            if t == len(self.loops[0].roll):
-                t = 0
             sleep(self.delay)
+
+        for l in self.loops:
+            l.mute()
+
+
+
+def scale2ints(minguscale, key='C', span=2, octave=3):
+    scale = minguscale(key, span)
+    notas = []
+    for o in range(octave, octave+span):
+        for n in range(7):
+            notas.append(int(Note(scale.ascending()[n], o)))
+    notas.append(int(Note(scale.tonic, octave+span)))
+    return notas
--- try.py
+++ try.py
@@ -1,51 +1,59 @@
-from loop import Loop, Sequencer
+from loop import Loop, Sequencer, scale2ints
 import mido
 import random
+import mingus.core.scales as scales
 
 #
 # timidity -iA -B2,8 -Os
-# 
+#
 #mido.set_backend('mido.backends.portmidi')
 
 print(mido.get_output_names())
 
 pianito = mido.open_output(mido.get_output_names()[1])
-pianito.send(mido.Message('program_change', program=69))
+pianito.send(mido.Message('program_change', program=48))
 
 sepa = mido.open_output(mido.get_output_names()[2])
-sepa.send(mido.Message('program_change', program=115))
+sepa.send(mido.Message('program_change', program=45))
 
-pong = mido.open_output(mido.get_output_names()[5])
-pong.send(mido.Message('program_change', program=21))
-
+cM = scale2ints(scales.Major, 'C', span=4, octave=2)
+am = scale2ints(scales.NaturalMinor, 'A', span=4, octave=3)
 
 l0 = Loop(roll=[[1, 0, 0, 0, 0, 0, 0, 1],
                 [0, 1, 0, 0, 0, 1, 0, 0],
                 [0, 0, 1, 0, 1, 0, 0, 0],
                 [0, 0, 0, 1, 0, 0, 0, 0]],
-          scale=range(70,79),
+          scale=am,
           instrument=sepa,
           name='sepa')
 
 l1 = Loop(roll=[[1, 0, 0, 0],
                 [1, 1, 1, 1],
                 [1, 0, 0, 1],
-                [1, 0, 1, 1],
+                [1, 0, 1, 0],
                 [0, 0, 1, 1],
-                [1, 1, 1, 0],
+                [1, 1, 0, 1],
                 [1, 0, 1, 0]],
-          scale=[41,52,55,70],
+          scale=cM,
           instrument=pianito,
           name='pian')
 
-l2 = Loop(roll=[[1, 0, 0, 0, 1, 0, 0, 1],
-                [0, 1, 0, 0, 1, 1, 0, 0],
-                [0, 0, 1, 0, 1, 1, 1, 0],
-                [0, 0, 0, 1, 0, 1, 0, 1]],
-          scale=range(60,69),
-          instrument=pong,
-          name='pong')
 
-s = Sequencer()
-s.loops = [l2, ]#l0, l1, l2]
-s.play()
+s = Sequencer(loops = [l0, l1],
+              bpm=120)
+
+
+while True:
+    if random.choice([True, False, False]):
+        l1.roll = [[random.choice([1,0,0]) for n in range(4)] for m in range(random.randint(2,6))]
+        l0.roll = [[random.choice([1,0,0]) for n in range(8)] for m in range(random.randint(3,7))]
+    if random.choice([True, False]):
+        scale1 = scale2ints(scales.NaturalMinor, random.choice(['A','D','E']), span=3, octave=random.randint(2,4))
+        scale2 = scale2ints(scales.NaturalMinor, random.choice(['D','E','B']), span=3, octave=random.randint(2,5))
+        l1.scale = scale1
+        l0.scale = scale2
+    else:
+        l1.scale = [n+2 for n in l0.scale]
+        l0.scale = [n+2 for n in l0.scale]
+        
+    s.play()    
