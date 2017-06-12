314369c45d51d7ae452c0a0f8cc82122cf32a971
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-06-11 20:56:46 -0500

    una instrumentacion bien bonita y escalas cambiantes

--- try.py
+++ try.py
@@ -10,50 +10,49 @@ import mingus.core.scales as scales
 
 print(mido.get_output_names())
 
-pianito = mido.open_output(mido.get_output_names()[1])
-pianito.send(mido.Message('program_change', program=48))
+muted = mido.open_output(mido.get_output_names()[1])
+muted.send(mido.Message('program_change', program=28))
 
-sepa = mido.open_output(mido.get_output_names()[2])
-sepa.send(mido.Message('program_change', program=45))
+glocken = mido.open_output(mido.get_output_names()[2])
+glocken.send(mido.Message('program_change', program=9))
 
 cM = scale2ints(scales.Major, 'C', span=4, octave=2)
 am = scale2ints(scales.NaturalMinor, 'A', span=4, octave=3)
 
-l0 = Loop(roll=[[1, 0, 0, 0, 0, 0, 0, 1],
-                [0, 1, 0, 0, 0, 1, 0, 0],
-                [0, 0, 1, 0, 1, 0, 0, 0],
-                [0, 0, 0, 1, 0, 0, 0, 0]],
-          scale=am,
-          instrument=sepa,
-          name='sepa')
+l0 = Loop(roll=[[1, 0, 0, 0, 0, 0, 0, 1]],
+          scale=[1,2],
+          instrument=glocken,
+          name='glocken')
 
 l1 = Loop(roll=[[1, 0, 0, 0],
-                [1, 1, 1, 1],
-                [1, 0, 0, 1],
-                [1, 0, 1, 0],
-                [0, 0, 1, 1],
-                [1, 1, 0, 1],
                 [1, 0, 1, 0]],
-          scale=cM,
-          instrument=pianito,
-          name='pian')
+          scale=[33,44,55],
+          instrument=muted,
+          name='muted')
 
-
-s = Sequencer(loops = [l0, l1],
-              bpm=120)
-
+s = Sequencer(loops = [l0, l1, ],
+              bpm=240)
 
 while True:
     if random.choice([True, False, False]):
-        l1.roll = [[random.choice([1,0,0]) for n in range(4)] for m in range(random.randint(2,6))]
-        l0.roll = [[random.choice([1,0,0]) for n in range(8)] for m in range(random.randint(3,7))]
+        l0.roll = [[random.choice([1,1,0]) for n in range(8)] for m in range(random.randint(4,7))]
+        l1.roll = [[random.choice([1,0,0]) for n in range(4)] for m in range(random.randint(3,6))]        
     if random.choice([True, False]):
-        scale1 = scale2ints(scales.NaturalMinor, random.choice(['A','D','E']), span=3, octave=random.randint(2,4))
-        scale2 = scale2ints(scales.NaturalMinor, random.choice(['D','E','B']), span=3, octave=random.randint(2,5))
-        l1.scale = scale1
-        l0.scale = scale2
+        l0.scale = scale2ints(random.choice([scales.Bachian,
+                                           scales.Phrygian]),
+                              random.choice(['A','A', 'E','B']),
+                              span=4,
+                              octave=random.randint(3,6))
+
+        l1.scale = scale2ints(random.choice([scales.Aeolian,
+                                             scales.Major]),
+                              random.choice(['C','C', 'G', 'G','D']),
+                              span=4,
+                              octave=random.randint(3,5))
     else:
-        l1.scale = [n+2 for n in l0.scale]
-        l0.scale = [n+2 for n in l0.scale]
+        delta=random.choice([-2, 2])
+        delta1=random.choice([-2, 2])        
+        l1.scale = [n+delta for n in l0.scale]
+        l0.scale = [n+delta1 for n in l0.scale]
         
     s.play()    
